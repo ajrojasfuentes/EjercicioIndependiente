fe0a8ed996b538946d90ec3fddda22dc9423f897
AnthonyRF00 <anthony.rf00@gmail.com>
2024-10-04 23:18:18 -0600

    Program generated with source code and modified

---
+++ my-app/src/app/sorted-list-of-immutables.model.spec.ts
@@ -0,0 +1,7 @@
+import { SortedListOfImmutables } from './sorted-list-of-immutables.model';
+
+describe('SortedListOfImmutables', () => {
+  it('should create an instance', () => {
+    expect(new SortedListOfImmutables()).toBeTruthy();
+  });
+});
